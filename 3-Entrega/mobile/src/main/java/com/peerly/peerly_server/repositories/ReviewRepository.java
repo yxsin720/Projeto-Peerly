fb063be91e6ab9a21d40a45ad68df0270d28191d
HamiltonPrado <francisco34baptista@gmail.com>
2025-12-14 21:01:23 +0000

    Limpar mobile: remover ficheiros que não pertencem ao Android Studio

--- 3-Entrega/mobile/src/main/java/com/peerly/peerly_server/repositories/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.peerly.peerly_server.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.peerly.peerly_server.models.responses.Review;
-import java.util.List;
-
-public interface ReviewRepository extends JpaRepository<Review, String> {
-    List<Review> findByTutorId(String tutorId);
-}
--- 3-Entrega/mobile/src/main/java/com/peerly/peerly_server/repositories/TutorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.peerly.peerly_server.repositories;
-
-import com.peerly.peerly_server.models.Tutor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TutorRepository extends JpaRepository<Tutor, String> {}
